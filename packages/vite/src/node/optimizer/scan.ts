1a915d60159c3afe71cab660baf665158132dd56
Nate Moore <nate@skypack.dev>
2021-12-15 10:08:07 -0600

    fix: exclude `src={expression}` from dep-scan

--- packages/vite/src/node/optimizer/scan.ts
+++ packages/vite/src/node/optimizer/scan.ts
@@ -147,10 +147,10 @@ const scriptModuleRE =
   /(<script\b[^>]*type\s*=\s*(?:"module"|'module')[^>]*>)(.*?)<\/script>/gims
 export const scriptRE = /(<script\b(?:\s[^>]*>|>))(.*?)<\/script>/gims
 export const commentRE = /<!--(.|[\r\n])*?-->/
-const srcRE = /\bsrc\s*=\s*(?:"([^"]+)"|'([^']+)'|([^\s'">]+))/im
-const typeRE = /\btype\s*=\s*(?:"([^"]+)"|'([^']+)'|([^\s'">]+))/im
-const langRE = /\blang\s*=\s*(?:"([^"]+)"|'([^']+)'|([^\s'">]+))/im
-const contextRE = /\bcontext\s*=\s*(?:"([^"]+)"|'([^']+)'|([^\s'">]+))/im
+const srcRE = /\bsrc\s*=\s*(?!\{)(?:"([^"]+)"|'([^']+)'|([^\s'">]+))/im
+const typeRE = /\btype\s*=\s*(?!\{)(?:"([^"]+)"|'([^']+)'|([^\s'">]+))/im
+const langRE = /\blang\s*=\s*(?!\{)(?:"([^"]+)"|'([^']+)'|([^\s'">]+))/im
+const contextRE = /\bcontext\s*=\s*(?!\{)(?:"([^"]+)"|'([^']+)'|([^\s'">]+))/im
 
 function esbuildScanPlugin(
   config: ResolvedConfig,
